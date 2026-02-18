06560caa6239ef6aa4462e742c913f25f3f2b8ba
agnonym <contact@agnonym.com>
2026-02-18 17:21:57 +0100

    + | Component - Studio - Layout

---
+++ app/components/Studio/Layout.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="h-screen flex flex-col bg-navy-950 text-white overflow-hidden">
+    <slot name="toolbar" />
+    <Splitter class="flex-1 min-h-0 !border-0 !bg-transparent">
+      <SplitterPanel :size="40" :min-size="20" class="min-h-0 overflow-auto">
+        <slot name="editor" />
+      </SplitterPanel>
+      <SplitterPanel :size="60" :min-size="30" class="min-h-0 overflow-hidden">
+        <slot name="panel" />
+      </SplitterPanel>
+    </Splitter>
+  </div>
+</template>
